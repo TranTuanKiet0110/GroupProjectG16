6a52a3def59049a753bbaf7fdd7656653a28229c
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2023-08-24 21:07:39 +0700

    edit AdminSellerManagement.js

--- src/pages/AdminSellerManagement.js
+++ src/pages/AdminSellerManagement.js
@@ -2,15 +2,8 @@ import React from 'react'
 // import Header from '../components/Header'
 import "../css/admin.css";
 import Sidebar from '../components/Sidebar';
-// import home from '../img/home.png';
-// import product from '../img/product.png';
-// import seller from '../img/seller.png';
 import menu from '../img/menu.png';
 import admin from '../img/admin.png';
-import customer from '../img/customer.png';
-import pendingSeller from '../img/pending_seller.png';
-import approvedSeller from '../img/approved_seller.png';
-import categories from '../img/categories.png';
 
 export default function AdminSellerManagement() {
     return (
@@ -33,51 +26,11 @@ export default function AdminSellerManagement() {
                 </header>
 
                 <main>
-                    <div className="cards">
-                        <div className="card">
-                            <div>
-                                <h1>10</h1>
-                                <span>Customers</span>
-                            </div>
-                            <div>
-                                <img src={customer} width="30px" height="30px" alt="Customer" />
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div>
-                                <h1>10</h1>
-                                <span>Pending sellers</span>
-                            </div>
-                            <div>
-                                <img src={pendingSeller} width="30px" height="30px" alt="Pending seller" />
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div>
-                                <h1>10</h1>
-                                <span>Approved sellers</span>
-                            </div>
-                            <div>
-                                <img src={approvedSeller} width="30px" height="30px" alt="Approved seller" />
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div>
-                                <h1>10</h1>
-                                <span>Categories</span>
-                            </div>
-                            <div>
-                                <img src={categories} width="30px" height="30px" alt="Categories" />
-                            </div>
-                        </div>
-                    </div>
-
                     <div className="sellers-management">
                         <div className="accounts">
                             <div className="card">
                                 <div className="card-header">
                                     <h2>Sellers Management</h2>
-                                    <button>See more</button>
                                 </div>
 
                                 <div className="card-body">
@@ -88,6 +41,7 @@ export default function AdminSellerManagement() {
                                                     <td>Name</td>
                                                     <td>Email</td>
                                                     <td>Phone number</td>
+                                                    <td>Business name</td>
                                                     <td>Status</td>
                                                 </tr>
                                             </thead>
@@ -96,6 +50,7 @@ export default function AdminSellerManagement() {
                                                     <td>Kiet</td>
                                                     <td>s3879300@rmit.edu.vn</td>
                                                     <td>0913638494</td>
+                                                    <td>Kiet123</td>
                                                     <td>
                                                         <span className="status green"></span>
                                                         Approved
@@ -105,6 +60,7 @@ export default function AdminSellerManagement() {
                                                     <td>Khanh</td>
                                                     <td>s3804620@rmit.edu.vn</td>
                                                     <td>0123456789</td>
+                                                    <td>Khanh123</td>
                                                     <td>
                                                         <span className="status green"></span>
                                                         Approved
@@ -114,6 +70,7 @@ export default function AdminSellerManagement() {
                                                     <td>Minh</td>
                                                     <td>s3879953@rmit.edu.vn</td>
                                                     <td>0123456789</td>
+                                                    <td>Khanh123</td>
                                                     <td>
                                                         <span className="status red"></span>
                                                         Pending
