556a38c6c076b1fd38126f555e23637f5ca16df6
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2023-09-02 21:28:09 +0700

    edit some admin dashboard components

--- src/api/sellers.js
+++ src/api/sellers.js
@@ -2,6 +2,7 @@ const sellers = [
     { id: 1, name: 'Kiet', email: 's3879300@rmit.edu.vn', phone: '0123456789', businessName: 'Kiet123', status: 'approved' },
     { id: 2, name: 'Khanh', email: 's3804620@rmit.edu.vn', phone: '0123456789', businessName: 'Khanh123', status: 'approved' },
     { id: 3, name: 'Minh', email: 's3879953@rmit.edu.vn', phone: '0123456789', businessName: 'Minh123', status: 'pending' },
+    { id: 4, name: 'Khoa', email: 's1234567@rmit.edu.vn', phone: '0123456789', businessName: 'Khoa123', status: 'pending' },
 ];
 
 // get all sellers (all attributes)
