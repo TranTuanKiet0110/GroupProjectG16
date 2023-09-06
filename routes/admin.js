70a3366695a2529f0d442a48e9d6b1f3f70427e3
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2023-09-06 10:05:07 +0700

    add seller model

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,14 @@ router.get("/dashboard", (req, res) => {
     res.send("Home Page")
 });
 
+//get category
+router.get("/category", (req, res) => {
+
+});
+
+// get seller management
+router.get("/sellerManagement", (req, res) => {
+    
+});
+
 module.exports = router;
