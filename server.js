3f1e64c79768e69f7ec78a1778194636c450e8fa
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2023-09-06 09:03:08 +0700

    create server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { MongoClient } = require("mongodb");
+const url = 'mongodb://localhost/';
+const client = new MongoClient(url);
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Connect to MongoDB");
+        await client.close();
+    } catch (err) {
+        console.log("Error connecting to MongoDB:", err);
+    }
+}
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server start on port ${port}`);
+});
+
+connect();
