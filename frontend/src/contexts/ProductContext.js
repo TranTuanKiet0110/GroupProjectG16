113492411cf92d260632219e6d4f847044460724
KietDuong <108349102+kietduong23@users.noreply.github.com>
2023-09-10 08:46:03 +0700

    Create ProductContext.js

---
+++ frontend/src/contexts/ProductContext.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { createContext } from "react";
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
+
+
+export const ProductContext = createContext();
+
+const ProductContextProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+
+    const loadProducts = async () => {
+        try {
+            const res = await axios.get(`${API_URL}/products`);
+            setProducts(res.data.products);
+        } catch (error) {
+            return {success: false, msg: error.message};
+        }
+    }
+
+    useEffect(() => loadProducts, []);
+
+    const productContextData = {products};
+
+    return (
+        <ProductContext.Provider value={productContextData}>{children}</ProductContext.Provider>
+    )
+}
+
+export default ProductContextProvider
