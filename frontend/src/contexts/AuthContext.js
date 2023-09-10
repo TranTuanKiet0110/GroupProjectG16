ddc7020fe493e32c09cd0c3abf4c1b579aa07454
KietDuong <108349102+kietduong23@users.noreply.github.com>
2023-09-10 08:45:50 +0700

    Create User authentication context

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { createContext } from "react";
+import axios from 'axios';
+const API_URL = 'http://localhost:8080';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+    const [authState, setAuthState] = useState({
+        user: null,
+        isAuthenticated: false,
+        loading: true
+    })
+    
+    const doCustomerLogin = async (loginData) => {
+        try {
+            const res = await axios.post(`${API_URL}/auth/login/customer`, loginData);
+            if (res.data.success) {
+                setAuthState({
+                    user: res.data.customer,
+                    isAuthenticated: true,
+                    loading: false
+                })
+            }
+            return { success: true, message: "Successfully logged in" }
+        } catch (error) {
+            return { success: false, message: error.message }
+        }
+    }
+    const authData = { authState, doCustomerLogin }
+    
+    return (
+        <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider
