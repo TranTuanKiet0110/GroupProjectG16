fafdf63bcec49edec10d589723b229968471ca4c
KietDuong <108349102+kietduong23@users.noreply.github.com>
2023-09-11 09:29:36 +0700

    Re-organize code in customer context

--- frontend/src/contexts/CustomerContext.js
+++ frontend/src/contexts/CustomerContext.js
@@ -3,17 +3,39 @@ import { createContext } from "react";
 import axios from 'axios';
 import { AuthContext } from './AuthContext';
 const API_URL = 'http://localhost:8080';
+const SHOPPING_CART_NAME = "MY-SHOPPING-CART";
 
 export const CustomerContext = createContext();
 
 const CustomerContextProvider = ({ children }) => {
-    const { authState } = useContext(AuthContext);
+    const { authState, doLogout } = useContext(AuthContext);
     const { user } = authState;
     const [shoppingCart, setShoppingCart] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
     const [orders, setOrders] = useState([]);
 
-    const SHOPPING_CART_NAME = "MY_SHOPPING_CART";
+    useEffect(() => {
+        if (user !== null) {
+            setShoppingCart(user.shoppingCart);
+            let currentTotalPrice = 0;
+            user.shoppingCart.forEach(item => {
+                currentTotalPrice += item.product.price * item.quantity;
+            });
+            setTotalPrice(currentTotalPrice);
+            setOrders(user.orders);
+        } else {
+            if (localStorage[SHOPPING_CART_NAME]) {
+                const jsonData = localStorage.getItem(SHOPPING_CART_NAME);
+                const shoppingCart = JSON.parse(jsonData);
+                setShoppingCart(shoppingCart);
+                let currentTotalPrice = 0;
+                shoppingCart.forEach(item => {
+                    currentTotalPrice += item.product.price * item.quantity;
+                });
+                setTotalPrice(currentTotalPrice);
+            }
+        }
+    }, [user])
 
     const loadShoppingCart = async () => {
         try {
@@ -27,200 +49,133 @@ const CustomerContextProvider = ({ children }) => {
                 });
                 setTotalPrice(currentTotalPrice);
             }
-            // if (res.data.shoppingCart.length == 0) {
-            //     if (localStorage.getItem(SHOPPING_CART_NAME) !== null) {
-            //         const jsonData = localStorage.getItem(SHOPPING_CART_NAME);
-            //         const shoppingCart = JSON.parse(jsonData);
-            //         setShoppingCart(shoppingCart);
-            //         let currentTotalPrice = 0;
-            //         shoppingCart.forEach(item => {
-            //             currentTotalPrice += item.product.price * item.quantity;
-            //         });
-            //         setTotalPrice(currentTotalPrice);
-            //     } else {
-            //         setShoppingCart([]);
-            //         setTotalPrice(0);
-            //     }
-            // } else {
-            //     // Convert to JSON
-            //     const cartData = JSON.stringify(res.data.shoppingCart);
-            //     // Store new data in Local Storage
-            //     localStorage.setItem(SHOPPING_CART_NAME, cartData);
-
-            //     setShoppingCart(res.data.shoppingCart);
-            //     let currentTotalPrice = 0;
-            //     shoppingCart.forEach(item => {
-            //         currentTotalPrice += item.product.price * item.quantity;
-            //     });
-            //     setTotalPrice(currentTotalPrice);
-            // }
         } catch (error) {
             return { success: false, msg: error.message }
         }
     }
 
-    useEffect(() => loadShoppingCart, [user]);
+    const handleCustomerLogout = () => {
+        doLogout();
+        setShoppingCart([]);
+        setTotalPrice(0)
+        setOrders([])
+    }
 
     const saveShoppingCart = (shoppingCart) => {
-        // console.log(shoppingCart);
         if (shoppingCart.length < 1) {
             localStorage.removeItem(SHOPPING_CART_NAME);
             setTotalPrice(0);
-        } else {
-            // Convert to JSON
-            const cartData = JSON.stringify(shoppingCart);
-            // Store new data in Local Storage
-            localStorage.setItem(SHOPPING_CART_NAME, cartData);
+            setShoppingCart([]);
+            return;
         }
-    }
+        setShoppingCart(shoppingCart);
+        let currentTotalPrice = 0;
+        shoppingCart.forEach(item => {
+            currentTotalPrice += item.product.price * item.quantity;
+        });
+        setTotalPrice(currentTotalPrice);
 
-    // const handleAddToCart = (product) => {
-    //     const existingProduct = shoppingCart.find((item) => item.product.id === product.id);
-    //     if (existingProduct === undefined) {
-    //         const newCart = [...shoppingCart, { product: product, quantity: 1 }];
-    //         setShoppingCart(newCart);
-    //         let newTotal = totalPrice + product.price;
-    //         setTotalPrice(newTotal);
-    //         // console.log(shoppingCart)
-    //         saveShoppingCart(newCart);
-    //     }
-    // }
+        // Convert to JSON
+        const cartData = JSON.stringify(shoppingCart);
+        // Store new data in Local Storage
+        localStorage.setItem(SHOPPING_CART_NAME, cartData);
+
+    }
 
     const handleAddToCart = async (product) => {
-        try {
-            const res = await axios.post(`${API_URL}/customers/${user._id}/cart`, { productID: product._id, quantity: 1 })
-            if (res.data.success) {
-                const newCartItem = res.data.item;
-                // setShoppingCart([...shoppingCart, newCartItem]);
-                let newTotal = totalPrice + newCartItem.product.price;
-                setTotalPrice(newTotal);
-                loadShoppingCart();
-                // saveShoppingCart(newCart);
+        if (user !== null) {
+            try {
+                const res = await axios.post(`${API_URL}/customers/${user._id}/cart`, { productID: product._id, quantity: 1 })
+                if (res.data.success) {
+                    saveShoppingCart(res.data.cart);
+                    await loadShoppingCart();
+                }
+                return res.data;
+            } catch (error) {
+                return { success: false, msg: error.message }
             }
-            return res.data;
-        } catch (error) {
-            return { success: false, msg: error.message }
+        } else {
+            const existingProduct = shoppingCart.find((item) => item.product._id === product._id);
+            if (existingProduct === undefined) {
+                const newCart = [...shoppingCart, { _id: product._id, product: product, quantity: 1 }];
+                saveShoppingCart(newCart);
+            }
         }
     }
-
-    // const handleClearCart = () => {
-    //     setTotalPrice(0);
-    //     setShoppingCart([]);
-    //     localStorage.removeItem(SHOPPING_CART_NAME);
-    // }
 
     const handleClearCart = async () => {
         if (user !== null) {
             try {
                 const res = await axios.delete(`${API_URL}/customers/${user._id}/cart`);
                 if (res.data.success) {
-                    setTotalPrice(0);
-                    setShoppingCart([]);
-                    localStorage.removeItem(SHOPPING_CART_NAME);
+                    saveShoppingCart([]);
                 }
                 return res.data;
             } catch (error) {
                 return { success: false, msg: error.message }
             }
+        } else {
+            saveShoppingCart([]);
         }
     }
-
-    // const handleItemRemove = (productID) => {
-    //     let newTotal = totalPrice;
-    //     const newCart = shoppingCart.filter((item) => {
-    //         if (item.product.id !== productID) {
-    //             return item;
-    //         } else {
-    //             newTotal = totalPrice - (item.quantity * item.product.price);
-    //             setTotalPrice(newTotal);
-    //             return null;
-    //         }
-    //     });
-    //     setShoppingCart(newCart);
-    //     saveShoppingCart(newCart);
-    // }
 
     const handleItemRemove = async (itemID) => {
         if (user !== null) {
             try {
                 const res = await axios.delete(`${API_URL}/customers/${user._id}/cart/${itemID}`);
                 if (res.data.success) {
-                    let newTotal = totalPrice;
                     const newCart = shoppingCart.filter((item) => {
                         if (item._id !== itemID) {
                             return item;
                         } else {
-                            newTotal = totalPrice - (item.quantity * item.product.price);
-                            setTotalPrice(newTotal);
                             return null;
                         }
                     });
-                    setShoppingCart(newCart);
                     saveShoppingCart(newCart);
                 }
                 return res.data;
             } catch (error) {
                 return { success: false, msg: error.message }
             }
+        } else {
+            const newCart = shoppingCart.filter((item) => {
+                if (item._id !== itemID) {
+                    return item;
+                } else {
+                    return null;
+                }
+            });
+            saveShoppingCart(newCart);
         }
     }
 
-    // const handleItemIncrease = (itemID) => {
-    //     let newTotal = totalPrice
-    //     const newCart = shoppingCart.map((item) => {
-    //         if (item.product.id === productID) {
-    //             newTotal = totalPrice + item.product.price;
-    //             setTotalPrice(newTotal);
-    //             item.quantity = item.quantity + 1;
-    //         }
-    //         return ({ ...item });
-    //     });
-    //     setShoppingCart(newCart);
-    //     saveShoppingCart(newCart);
-    // }
-
     const handleItemIncrease = async (itemID, quantity) => {
         if (user !== null) {
-            console.log(itemID);
             try {
                 const res = await axios.put(`${API_URL}/customers/${user._id}/cart/${itemID}`, { quantity: quantity + 1 });
                 if (res.data.success) {
-                    let newTotal = totalPrice
                     const newCart = shoppingCart.map((item) => {
                         if (item._id === itemID) {
-                            newTotal = totalPrice + item.product.price;
-                            setTotalPrice(newTotal);
+
                             item.quantity = item.quantity + 1;
                         }
                         return ({ ...item });
                     });
-                    setShoppingCart(newCart);
                     saveShoppingCart(newCart);
                 }
             } catch (error) {
-                // if (error.response.data) {
-                //     return error.response.data
-                // }
                 return { success: false, msg: error.message }
             }
+        } else {
+            const newCart = shoppingCart.map((item) => {
+                if (item._id === itemID) {
+                    item.quantity = item.quantity + 1;
+                }
+                return ({ ...item });
+            });
+            saveShoppingCart(newCart);
         }
     }
-
-    // const handleItemDecrease = (itemID) => {
-    //     let newTotal = totalPrice;
-    //     const newCart = shoppingCart.map((item) => {
-    //         if (item.product.id === productID) {
-    //             if (item.quantity > 1) {
-    //                 newTotal = totalPrice - item.product.price;
-    //                 setTotalPrice(newTotal);
-    //                 item.quantity = item.quantity - 1;
-    //             }
-    //         }
-    //         return ({ ...item })
-    //     });
-    //     setShoppingCart(newCart);
-    //     saveShoppingCart(newCart);
-    // }
 
     const handleItemDecrease = async (itemID, quantity) => {
         if (user !== null) {
@@ -228,36 +183,30 @@ const CustomerContextProvider = ({ children }) => {
                 try {
                     const res = await axios.put(`${API_URL}/customers/${user._id}/cart/${itemID}`, { quantity: quantity - 1 });
                     if (res.data.success) {
-                        let newTotal = totalPrice;
                         const newCart = shoppingCart.map((item) => {
                             if (item._id === itemID) {
-                                newTotal = totalPrice - item.product.price;
-                                setTotalPrice(newTotal);
                                 item.quantity = item.quantity - 1;
                             }
                             return ({ ...item })
                         });
-                        setShoppingCart(newCart);
                         saveShoppingCart(newCart);
                     }
                 } catch (error) {
                     return { success: false, msg: error.message }
                 }
             }
+        } else {
+            const newCart = shoppingCart.map((item) => {
+                if (item._id === itemID) {
+                    if (item.quantity > 1) {
+                        item.quantity = item.quantity - 1;
+                    }
+                }
+                return ({ ...item })
+            });
+            saveShoppingCart(newCart);
         }
     }
-
-    // const loadOrders = () => {
-    //     //This is for testing
-    //     const orders = [{
-    //         items: [{
-    //             product: { id: 1, name: "iPhone", img: "https://techland.com.vn/wp-content/uploads/2021/09/iphone-13-pink-select-2021.png", price: 1000, description: "Apple iPhone", category: "Smart Phones" },
-    //             quantity: 1
-    //         }],
-    //         status: 'Shipped'
-    //     }]
-    //     setOrders(orders);
-    // }
 
     const loadOrders = async () => {
         try {
@@ -267,48 +216,23 @@ const CustomerContextProvider = ({ children }) => {
             }
             return res.data;
         } catch (error) {
-            // if (error.response.data) {
-            //     return error.response.data
-            // }
             return { success: false, msg: error.message }
         }
     }
-
-    useEffect(() => loadOrders, [user]);
-
-    // const handlePlaceOrder = () => {
-    //     const newOrder = {
-    //         items: shoppingCart,
-    //         status: 'New'
-    //     }
-    //     const newOrderList = [...orders, newOrder];
-    //     setOrders(newOrderList);
-    //     setShoppingCart([]);
-    //     saveShoppingCart([]);
-    //     console.log(newOrderList);
-    // }
 
     const handlePlaceOrder = async () => {
         if (user !== null) {
             try {
                 const res = await axios.post(`${API_URL}/orders`, { customerID: user._id });
                 if (res.data.success) {
-                    const newOrderList = [...orders, res.data.newOrder];
-                    // setOrders(newOrderList);
-                    setShoppingCart([]);
                     saveShoppingCart([]);
-                    console.log(newOrderList);
-                    loadOrders();
+                    await loadOrders();
                 }
             } catch (error) {
-                // if (error.response.data) {
-                //     return error.response.data
-                // }
                 return { success: false, msg: error.message }
             }
         }
     }
-
 
     const customerData = {
         shoppingCart,
@@ -320,7 +244,8 @@ const CustomerContextProvider = ({ children }) => {
         handleItemIncrease,
         handleItemDecrease,
         handlePlaceOrder,
-        handlePlaceOrder
+        handlePlaceOrder,
+        handleCustomerLogout
     }
     return (
         <CustomerContext.Provider value={customerData}>{children}</CustomerContext.Provider>
